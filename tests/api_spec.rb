462b7cdc3ad75776c6d35c7f78655aa5194aa355
sagarlekar <sagar.arlekar@gmail.com>
2019-04-20 23:06:04 +0000

    rspec

---
+++ tests/api_spec.rb
@@ -0,0 +1,15 @@
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+require 'api'
+
+describe Todoable::Api do
+    let(:methods) { %i(lists create get delete patch delete_item create_item finish_item) }
+    let(:subject) { Todoable::Api.new }
+    
+    it "defines the 8 methods" do
+        methods.each do |method|
+            expect(subject.respond_to? method.to_sym).to be_truthy
+        end
+    end
+    
+    #TODO Add specs to test Api methods with mocks
+end
